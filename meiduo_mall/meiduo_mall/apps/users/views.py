be45b1dffa72ec4d606e5705fdeb7013f8602124
ten@qq.com <782555894@qq.com>
2019-10-04 10:14:27 +0800

    用户注册后端逻辑-接收参数

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -12,3 +12,13 @@ class RegisterView(View):
         :return: 注册界面
         """
         return render(request, 'register.html')
+
+    def post(self, request):
+        """实现用户注册业务逻辑"""
+        """实现用户注册业务逻辑"""
+        # 接收参数：表单参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
