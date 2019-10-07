acbda1debc9d851ff04c89f2eb8ae1f840f2e7e9
ten@qq.com <782555894@qq.com>
2019-10-07 13:24:33 +0800

    用户登录之QQ登录-OAuth2.0认证获取openid

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,9 +1,46 @@
-from QQLoginTool.QQtool import OAuthQQ
+from django.shortcuts import render
 from django.views import View
+from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
 from django import http
+import logging
 
 from meiduo_mall.utils.response_code import RETCODE
+
+# Create your views here.
+
+
+# 创建日志输出器
+logger = logging.getLogger('django')
+
+
+class QQAuthUserView(View):
+    """处理QQ登录回调：oauth_callback"""
+
+    def get(self, request):
+        """处理QQ登录回调的业务逻辑"""
+
+        # 获取code
+        code = request.GET.get('code')
+        if not code:
+            return http.HttpResponseForbidden('获取code失败')
+
+        # 创建工具对象
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID, client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI)
+
+        try:
+            # 使用code获取access_token
+            access_token = oauth.get_access_token(code)
+
+            # 使用access_token获取openid
+            openid = oauth.get_open_id(access_token)
+        except Exception as e:
+            logger.error(e)
+            return http.HttpResponseServerError('OAuth2.0认证失败')
+
+        # 使用openid判断该QQ用户是否绑定过美多商城的用户
+        pass
 
 
 class QQAuthURLView(View):
