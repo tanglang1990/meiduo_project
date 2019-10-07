7dce1faec7e4d5d5b7cf0ba947e388ff210962b3
ten@qq.com <782555894@qq.com>
2019-10-07 13:36:40 +0800

    添加oAuth中openid与user的关联模型

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -15,7 +15,7 @@ import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, BASE_DIR)
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'oauth',
 ]
 
 
