6d52021ef0905c13df9e64561881adbe57f55832
ten@qq.com <782555894@qq.com>
2019-10-09 11:12:47 +0800

    定义发送邮件的异步任务

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -8,4 +8,4 @@ celery_app = Celery('meiduo')
 celery_app.config_from_object('celery_tasks.config')
 
 # 注册任务
-celery_app.autodiscover_tasks(['celery_tasks.sms'])
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
