0f088052cc5b758c6b0368c08fe2ee28fb770f6f
ten@qq.com <782555894@qq.com>
2019-10-10 00:54:26 +0800

    将省级模型转换成字典列表

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from django import http
+import logging
+
 from areas.models import Area
+from meiduo_mall.utils.response_code import RETCODE
 
-
-# Create your views here.
+logger = logging.getLogger('django')
 
 
 class AreasView(View):
@@ -16,10 +19,22 @@ class AreasView(View):
         if not area_id:
             # 查询省级数据
             # Area.objects.filter(属性名__条件表达式=值)
-            province_model_list = Area.objects.filter(parent__isnull=True)
+            try:
+                province_model_list = Area.objects.filter(parent__isnull=True)
 
-            # return http.JsonResponse({'code': '', 'errmsg': '', 'province_list': province_model_list})
-            pass
+                # 需要将模型列表转成字典列表
+                province_list = []
+                for province_model in province_model_list:
+                    province_dict = {
+                        "id": province_model.id,
+                        "name": province_model.name
+                    }
+                    province_list.append(province_dict)
+                # 响应省级JSON数据
+                return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': province_list})
+            except Exception as e:
+                logger.error(e)
+                return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '查询省份数据错误'})
         else:
             # 查询城市或区县数据
             pass
