d1a3c5dd81f269dee539fe27fdfca8d053212734
ten@qq.com <782555894@qq.com>
2019-10-11 01:34:54 +0800

    收获地址完成

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
     url(r'^addresses/$', views.AddressView.as_view(), name='address'),
     # 新增用户地址
     url(r'^addresses/create/$', views.AddressCreateView.as_view()),
+    # 更新和删除地址
+    url(r'^addresses/(?P<address_id>\d+)/$', views.UpdateDestoryAddressView.as_view()),
+    # 设置默认地址
+    url(r'^addresses/(?P<address_id>\d+)/default/$', views.DefaultAddressView.as_view()),
+    # 更新地址标题
+    url(r'^addresses/(?P<address_id>\d+)/title/$', views.UpdateTitleAddressView.as_view()),
 ]
