17b867ef13fb4706c441c8bb908c1cea3de961b7
ten@qq.com <782555894@qq.com>
2019-10-12 16:01:13 +0800

    展示首页商品广告

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -3,13 +3,18 @@ from django.views import View
 from collections import OrderedDict
 
 from goods.models import GoodsChannelGroup, GoodsChannel, GoodsCategory
+from contents.models import ContentCategory, Content
+
+
+# Create your views here.
 
 
 class IndexView(View):
     """首页广告"""
 
     def get(self, request):
-        """提供首页广告页面"""  # 查询并展示商品分类
+        """提供首页广告页面"""
+        # 查询并展示商品分类
         # 准备商品分类对应的字典
         categories = OrderedDict()
         # 查询所有的商品频道:37个一级类别
@@ -30,4 +35,29 @@ class IndexView(View):
                 'name': cat1.name,
                 'url': channel.url
             })
-            return render(request, 'index.html')
+
+            # 查询二级和三级类别
+            for cat2 in cat1.subs.all():  # 从一级类别找二级类别
+                cat2.sub_cats = []  # 给二级类别添加一个保存三级类别的列表
+                for cat3 in cat2.subs.all():  # 从二级类别找三级类别
+                    cat2.sub_cats.append(cat3)  # 将三级类别添加到二级sub_cats
+
+                # 将二级类别添加到一级类别的sub_cats
+                categories[group_id]['sub_cats'].append(cat2)
+
+        # 查询首页广告数据
+        # 查询所有的广告类别
+        contents = OrderedDict()
+        content_categories = ContentCategory.objects.all()
+        for content_category in content_categories:
+            # 使用广告类别查询出该类别对应的所有的广告内容
+            contents[content_category.key] = content_category.content_set.filter(status=True).order_by(
+                'sequence')  # 查询出未下架的广告并排序
+
+        # 构造上下文
+        context = {
+            'categories': categories,
+            'contents': contents
+        }
+
+        return render(request, 'index.html', context)
